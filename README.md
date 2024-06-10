36f478352881ce07c28e439abfe5706710e5dda3
furatto <furatto570@gmail.com>
2024-06-10 20:31:00 +0900

    change README&py

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # minecraft_server_bot
 discord.bot作成用
+
+完成予想
+![kansei yosou](/img/project.png)
---
+++ discord.py
@@ -0,0 +1,41 @@
+import discord
+from mcstatus import MinecraftServer
+import os
+
+TOKEN = "Token"
+
+MINECRAFT_SERVER_DOMAIN = "ServerDomain"
+MINECRAFT_SERVER_PORT = "Port"
+
+# Botのインスタンスを作成
+intents = discord.Intents.default()
+intents.messages = True
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!status'):
+        try:
+            # Minecraftサーバのステータスを取得
+            server = MinecraftServer.lookup(f"{MINECRAFT_SERVER_DOMAIN}:{MINECRAFT_SERVER_PORT}")
+            status = server.status()
+            
+            # メッセージを作成
+            response = (
+                f"サーバはオンラインです！\n"
+                f"プレイヤー数: {status.players.online}/{status.players.max}\n"
+                f"ラグ: {status.latency}ms"
+            )
+        except Exception as e:
+            response = f"サーバに接続できませんでした。エラー: {e}"
+
+        await message.channel.send(response)
+
+client.run(TOKEN)
